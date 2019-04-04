86aeacfb946b3ff0ca582e333f10105b16d1922f
Marv_Tron <marvinatryonjr@gmail.com>
2019-04-03 21:54:18 -0700

    oh yes daddy

---
+++ server.js
@@ -0,0 +1,73 @@
+require("dotenv").config();
+var express = require("express");
+var exphbs = require("express-handlebars");
+var db = require("./models");
+var PORT = process.env.PORT || 3000;
+var app = express();
+var passport = require("passport");
+var session = require("express-session");
+//Models
+var models = require("./models");
+//load passport strategies
+require("./config/passport.js")(passport, models.user);
+//Sync Database
+models.sequelize
+  .sync()
+  .then(function() {
+    console.log("Nice! Database looks fine");
+  })
+  .catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!");
+  });
+
+app.get("/", function(req, res) {
+  res.send("Welcome to VTS");
+});
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Middleware For Passport
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+// Handlebars
+app.set("view engine", "handlebars");
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+
+// Routes
+// eslint-disable-next-line no-unused-vars
+var authRoute = require("./routes/auth")(app, passport);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
+
+//Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
+
+module.exports = app;
